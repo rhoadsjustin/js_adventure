22971a9b418f4d1eefef6035d5c8bf9515a74b34
Justin Rhoads <rhoadsjustin@gmail.com>
2017-05-02 10:40:11 -0500

    added random number generator to batman tree, and started Captain America tree.

--- adventure.js
+++ adventure.js
@@ -20,7 +20,7 @@ var superHero = prompt("If you could be one of the following Superheroes who wou
               } else if (ironMan_2 == "women") {
                   console.log("Niceee");
               }
-      } else if (ironMan_1 == "the mech suit") {
+       else if (ironMan_1 == "the mech suit") {
          var ironMan_3 = prompt("The world is in trouble, what would you choose to do? (Save Penny, Save the World, or Die trying to do both")
        if(ironMan_3 == "Save Penny") {
            alert("Choosing one over the world? Interesting choice, must be love.")
@@ -32,7 +32,7 @@ var superHero = prompt("If you could be one of the following Superheroes who wou
       }
 
   // Superman decision tree
-  if (superHero == "Superman") {
+} else  if (superHero == "Superman") {
         var superMan_1 = prompt("Which of his superpowers made you pick him? (Xray Vision, Flight, or Lasers)")
        if(superMan_1 == "Xray Vision") {
        alert("Not a bad choice. Use this power wisely, but I can see through your choice that you should play again.");
@@ -66,15 +66,38 @@ var superHero = prompt("If you could be one of the following Superheroes who wou
        }
 
        // Batman Decision Tree
-       if(superMan_2 == "cars") {
-         console.log("Zoom Zoom");
-       } else {
-         console.log("Niceee");
+  } else if(superHero == "Batman") {
+         var batman_1 = prompt("Well hello Mr. Wayne. Hopefully you're not Ben Affleck cause then you're the worst Batman. But Anyways, back to our questions. Gotham is in trouble, what do you do? (Hang with Alfred, Hop in the Batmobile, or Hop in the Lambo)")
+         if (batman_1 == "Hang with Alfred") {
+           alert("While Alfred is a dope old man with a sweet accent. You let Gotham down, shame on you Bruce. Game Over.");
+         } else if (batman_1 == "Hop in the Batmobile") {
+
+                  var batman_1a = prompt("Honorable choice, now you get to go Karate chop some bad guys. The have taken hostages in a warehouse, how do you enter the warehouse? (Through the front door batarangs a blazing, A conviently unlocked sky-light, or drive the Batmobile through the wall)")
+                  if (batman_1a == "through the front door batarangs a blazing") {
+                    alert("You are Batman, not superman. You can die, and you did die. Game Over.")
+                  } else if (batman_1a == "A conviently unlocked sky-light") {
+                    alert("You are a master of stealth. You save the hostages and Gotham. You are a hero. You win!");
+                  } else if (batman_1a == "drive the Batmobile through the wall") {
+                    alert("Your confidence and ultimately your fearlessness strike fear in the enemy, they pass out from how awesome that was. Then you save the hostages. You are the hero and your epic in your decision making. You win.");
+                  } else {
+                    alert("Wrong.");
+                  }
+                }
+        else if (batman_1 == "Hop in the Lambo") {
+         var batman_1b = prompt("Respect the style and the flare, but Bruce Wayne isn't cut out for this. Only Batman can save Gotham. You lose. Bonus life if you can guess what number I'm thinking of between 1-10?");
+         var numberGuess = parseInt(batman_1b);
+         var randomNumber = Math.floor(Math.random() * 10);
+          if (numberGuess === randomNumber) {
+            alert(numberGuess + " = " + randomNumber + " Wow how'd you know.")
+          } else {
+            alert("Welp. You lost your last chance. My number was " + randomNumber + " You lose.")
+          }
        }
-  } else if(ironMan_1 == "the mech suit") {
-         var ironMan_3 = prompt("The world is in trouble, what would you choose to do? (Save Penny, Save the World, or Die trying to do both")
-       if(ironMan_3 == "Save Penny") {
-           console.log("Choosing one over the world? Interesting choice, must be love.")
+  } else if(superHero == "Captain America") {
+         var cptAmerica_1 = prompt("Wow, you are so patriotic. How many stars are on your sheild?")
+         var starsNumber = parseInt(cptAmerica_1)
+       if(starsNumber == 1) {
+           var cptAmerica_2 = prompt("Choosing one over the world? Interesting choice, must be love.")
          } else if(ironMan_3 == "Save the World") {
            console.log("Well technically Penny is apart of the world, so this is the right choice. Good job. You win.");
          } else {
